72fa1c6d550b17f652ff179c53abc37602f275a8
d.shishmakov <d.shishmakov@corp.nekki.ru>
2017-09-04 19:11:53 +0300

    Try change

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,9 @@
 # File deploy.sh
 echo "Start deploy"
 
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+curl -sSL https://get.rvm.io | bash -s stable
+
 # Download project
 git clone https://github.com/Artemmkin/reddit.git ~/reddit
 
