6fec3f8bfcbd3eee72f1fa578f7f61ed4cc89a4e
DmitriySh <dashishmakov@gmail.com>
2017-09-05 04:30:12 +0300

    Merge script changes

--- startup_script.sh
+++ startup_script.sh
@@ -1,39 +1,42 @@
+#!/bin/bash
+
+echo "Start install Ruby"
 
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable
-
-source ~/.rvm/scripts/rvmrvm requirements
-
+source ~/.rvm/scripts/rvm
+rvm requirements
 rvm install 2.4.1
 rvm use 2.4.1 --default
-
 gem install bundler -V --no-ri --no-rdoc
-
 ruby -v
 gem -v bundler
 
+echo "End install Ruby"
 
 
+
+echo "Start install MongoDB"
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
-
 sudo apt-get update
 sudo apt-get install -y mongodb-org
-
-
 sudo systemctl start mongod
 sudo systemctl enable mongod
-
 sudo systemctl status mongod
 
+echo "End install MongoDB"
 
 
 
-cd ~
-git clone https://github.com/Artemmkin/reddit.git
+echo "Start deploy"
 
-cd reddit && bundle install
-
+git clone https://github.com/Artemmkin/reddit.git ~/reddit
+cd ~/reddit
+source ~/.rvm/scripts/rvm
+bundle install
 puma -d
+
+echo "End deploy"
 
