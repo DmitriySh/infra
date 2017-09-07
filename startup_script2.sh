5a458bd8a1d33c220b7badf5478b372ffb45ab5c
DmitriySh <dashishmakov@gmail.com>
2017-09-07 07:23:49 +0300

    Fix install mongo

--- startup_script2.sh
+++ startup_script2.sh
@@ -24,6 +24,7 @@ echo "Start install MongoDB"
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
 sudo apt-get update
+sudo apt-get install -y mongodb-org
 sudo systemctl start mongod
 sudo systemctl enable mongod
 sudo systemctl status mongod
