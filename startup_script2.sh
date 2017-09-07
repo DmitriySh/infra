589a450ab4958b354eb9654a0ceb8b082995e6da
DmitriySh <dashishmakov@gmail.com>
2017-09-07 07:03:02 +0300

    Use reletive path for source

--- startup_script2.sh
+++ startup_script2.sh
@@ -9,7 +9,7 @@ echo path: `pwd`
 echo "Start install Ruby"
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable
-source /home/appuser/reddit/.rvm/scripts/rvm
+source ~/.rvm/scripts/rvm
 rvm requirements
 rvm install 2.4.1
 rvm use 2.4.1 --default
