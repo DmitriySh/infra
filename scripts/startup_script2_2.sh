cc23311bdcf479f9cb7faa9a0b10a2b372482106
DmitriySh <dashishmakov@gmail.com>
2017-09-11 06:04:36 +0300

    Fix startup deploy script

--- scripts/startup_script2_2.sh
+++ scripts/startup_script2_2.sh
@@ -6,8 +6,9 @@ echo user: `whoami`
 echo path: `pwd`
 
 echo "Start deploy"
-git clone https://github.com/Artemmkin/reddit.git
-cd reddit/
+source ~/.rvm/scripts/rvm
+git clone https://github.com/Artemmkin/reddit.git ~/reddit
+cd ~/reddit
 bundle install
 puma -d
 echo "End deploy"
