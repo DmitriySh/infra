fe4355db3849504dfa53608d879578ab6ecfcf3b
DmitriySh <dashishmakov@gmail.com>
2017-09-11 23:39:12 +0300

    Add provisioners for puma-service

--- scripts/startup_script2_1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo -u appuser git clone -b feature/immutable-os-packer  https://github.com/DmitriySh/infra.git /home/appuser/infra
-cd /home/appuser/infra
-
-sudo -u appuser bash scripts/startup_script2_2.sh
-
