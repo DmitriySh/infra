212a3e5975a0b8c79fa12f731980a0519afc2012
DmitriySh <dashishmakov@gmail.com>
2017-09-13 15:20:32 +0300

    Add resource 'firewall_puma' to open tcp port

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,3 +28,20 @@ resource "google_compute_instance" "app" {
   } 
 }
 
+resource "google_compute_firewall" "firewall_puma" {
+    name = "allow-puma-default"
+   # Название сети, в которой действует правило
+    network = "default"
+    
+    # Какой доступ разрешить
+    allow {
+       protocol = "tcp"
+       ports = ["9292"]
+    }
+
+    # Каким адресам разрешаем доступ
+    source_ranges = ["0.0.0.0/0"]
+    # Правило применимо для инстансов с тегом …
+    target_tags = ["reddit-app"]
+}
+
