9f0250c2a99636dcf175848fd5778d9168a7b188
DmitriySh <dashishmakov@gmail.com>
2017-09-13 18:39:28 +0300

    Reset maim.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,55 +1,24 @@
 provider "google" {
-  project = "infra-179717"
-  region  = "europe-west1"
+    project = "infra-179717"
+    region = "europe-west1"
 }
 
 resource "google_compute_instance" "app" {
-  name         = "reddit-app"
-  machine_type = "g1-small"
-  zone         = "europe-west1-b"
-  tags = ["reddit-app"]
-
-  metadata {
-    sshKeys = "appuser:${file("~/.ssh/otus_devops_appuser.pub")}"
-  } 
-  # определение загрузочного диска
-  boot_disk {
-    initialize_params {
-      image = "reddit-base-3-1505269146"
+    name = "reddit-app"
+    machine_type = "g1-small"
+    zone = "europe-west1-b"
+    # определение загрузочного диска
+    boot_disk {
+        initialize_params {
+            image = "reddit-base-3-1505269146"
+        }
     }
-  }
-  
-  # определение сетевого интерфейса
-  network_interface {
-    # сеть, к которой присоединить данный интерфейс
-    network = "default"
-    # использовать ephemeral IP для доступа из Интернет
-    access_config {}
-  } 
-
-  provisioner "file" {
-    source = "files/puma.service"
-    destination = "/tmp/puma.service"
-  }
-  provisioner "remote-exec" {
-    script = "files/deploy.sh"
-  }
-}
-
-resource "google_compute_firewall" "firewall_puma" {
-    name = "allow-puma-default"
-   # Название сети, в которой действует правило
-    network = "default"
-    
-    # Какой доступ разрешить
-    allow {
-       protocol = "tcp"
-       ports = ["9292"]
+    # определение сетевого интерфейса
+    network_interface {
+        # сеть, к которой присоединить данный интерфейс
+        network = "default"
+        # использовать ephemeral IP для доступа из Интернет
+        access_config {}
     }
-
-    # Каким адресам разрешаем доступ
-    source_ranges = ["0.0.0.0/0"]
-    # Правило применимо для инстансов с тегом …
-    target_tags = ["reddit-app"]
 }
 
