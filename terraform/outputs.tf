88300edd45503fa7344e6a13a1b9d28d1c8ed5c7
DmitriySh <dashishmakov@gmail.com>
2017-09-14 05:41:51 +0300

    Format terraform files

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,3 @@
 output "app_external_ip" {
-   value = "${google_compute_instance.app.network_interface.0.access_config.0.assigned_nat_ip}"
+  value = "${google_compute_instance.app.network_interface.0.access_config.0.assigned_nat_ip}"
 }
-
