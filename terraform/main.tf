d370473a47adced1e01ebfd007026a977efa3d1f
DmitriySh <dashishmakov@gmail.com>
2017-09-17 08:38:35 +0300

    Make formatting

--- terraform/main.tf
+++ terraform/main.tf
@@ -68,17 +68,18 @@ resource "google_compute_firewall" "firewall_puma" {
 }
 
 resource "google_compute_firewall" "firewall_ssh" {
-  name    = "default-allow-ssh"
-  network = "default"
+  name        = "default-allow-ssh"
+  network     = "default"
   description = "Allow SSH from anywhere"
+
   allow {
     protocol = "tcp"
     ports    = ["22"]
   }
+
   source_ranges = ["0.0.0.0/0"]
 }
 
 resource "google_compute_address" "app_ip" {
   name = "reddit-app-ip"
 }
-
