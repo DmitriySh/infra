07f54dec8b9ea2b21ff920f15adc660b457a2e45
DmitriySh <dashishmakov@gmail.com>
2017-09-17 17:14:02 +0300

    Revert variable 'private_key_path'

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,6 +11,10 @@ variable public_key_path {
   description = "Path to the public key used for ssh access"
 }
 
+variable private_key_path {
+  description = "Path to the private key used to connect to instance"
+}
+
 variable app_disk_image {
   description = "Disk image for reddit app"
   default     = "reddit-app-base"
