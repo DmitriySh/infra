a356a811599eae062238112a395362300d379566
DmitriySh <dashishmakov@gmail.com>
2017-09-17 07:36:54 +0300

    Add common rule ssh for 'default' network

--- terraform/main.tf
+++ terraform/main.tf
@@ -64,3 +64,15 @@ resource "google_compute_firewall" "firewall_puma" {
   # Правило применимо для инстансов с тегом
   target_tags = ["reddit-app"]
 }
+
+resource "google_compute_firewall" "firewall_ssh" {
+  name    = "default-allow-ssh"
+  network = "default"
+  description = "Allow SSH from anywhere"
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+}
+
