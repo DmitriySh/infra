0185a54d88dfbc8e179e93dfd16070e95b8b2674
DmitriySh <dashishmakov@gmail.com>
2017-09-17 07:54:05 +0300

    Add custom ip address 'reddit-app-ip'

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,7 +22,9 @@ resource "google_compute_instance" "app" {
     network = "default"
 
     # использовать ephemeral IP для доступа из Интернет
-    access_config {}
+    access_config = {
+      nat_ip = "${google_compute_address.app_ip.address}"
+    }
   }
 
   metadata {
@@ -74,5 +76,9 @@ resource "google_compute_firewall" "firewall_ssh" {
     ports    = ["22"]
   }
   source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_address" "app_ip" {
+  name = "reddit-app-ip"
 }
 
