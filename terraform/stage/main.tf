f416709a569d0814e5e451b3f0f323ae7ab10022
DmitriySh <dashishmakov@gmail.com>
2017-09-18 04:36:45 +0300

    Delete old terraform files

--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -4,18 +4,19 @@ provider "google" {
 }
 
 module app {
-  source = "./modules/app"
+  source = "../modules/app"
   public_key_path = "${var.public_key_path}"
   app_disk_image = "${var.app_disk_image}"  
 }
 
 module db {
-  source = "./modules/db"
+  source = "../modules/db"
   public_key_path = "${var.public_key_path}"
   db_disk_image = "${var.db_disk_image}"
 }
 
 module vpc {
-  source = "./modules/vpc"
+  source = "../modules/vpc"
+  source_ranges = ["0.0.0.0/0"]
 }
 
