fa339ce5feffaa781d5cd8e3c8f2e45bc909c015
DmitriySh <dashishmakov@gmail.com>
2017-09-20 13:25:59 +0300

    Change bucket id

--- terraform/prod/backend.tf
+++ terraform/prod/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket  = "shishmakov-bucket"
+    bucket  = "infra-179717-bucket"
     path    = "infra/terraform.tfstate"
     project = "infra-179717"
     region = "europe-west1"
